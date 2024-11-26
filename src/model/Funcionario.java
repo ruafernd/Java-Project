79948ab6896035e4051710a93e91fb2044c8a9a7
Kaline <150398224+MedeirosKaline@users.noreply.github.com>
2024-11-26 00:39:19 -0300

    Update Funcionario.java

--- src/model/Funcionario.java
+++ src/model/Funcionario.java
@@ -1,2 +1,12 @@
-public abstract class Funcionario {
+public class Funcionario extends Pessoa{
+    private String cargo;
+    public Funcionario (String nome, String cpf, String cargo){
+        super(nome,cpf);
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void exibirDados(){
+        System.out.println("Funcionário: " + getNome() +"-CPF:" + getCpf() +" -Cargo:" + cargo);
+    }
 }
