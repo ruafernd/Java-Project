a8de1fd59e979dc08a3620aa76b4b5edfa715d17
Kaline <150398224+MedeirosKaline@users.noreply.github.com>
2024-11-27 14:56:32 -0300

    Update ItemPedido.java

--- src/model/ItemPedido.java
+++ src/model/ItemPedido.java
@@ -1,3 +1,17 @@
 public class ItemPedido {
+    private Produto produto;
+    private int quantidade;
     
+    public ItemPedido(Produto produto, int quantidade) {
+    this.produto = produto;
+    this.quantidade = quantidade;
 }
+    
+  public double calcularSubtotal() {
+    return produto.getPreco() * quantidade;
+}
+    
+  public String getDescricao() {
+    return quantidade + "x " + produto.getNome();
+}
+ }
