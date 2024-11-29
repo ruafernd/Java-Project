a5ce85d408f09822deb96fbade7795f3cb7db01a
Kaline <150398224+MedeirosKaline@users.noreply.github.com>
2024-11-29 14:30:35 -0300

    Update Pedido.java

--- src/model/Pedido.java
+++ src/model/Pedido.java
@@ -1,3 +1,19 @@
-public class Pedido {
-    
+package model;
+
+public class ItemPedido {
+    private Produto produto;
+    private int quantidade;
+
+    public ItemPedido(Produto produto, int quantidade) {
+        this.produto = produto;
+        this.quantidade = quantidade;
+    }
+
+    public Produto getProduto() {
+        return produto;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
 }
